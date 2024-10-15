f474ee3dc17fc027d648a2ba26ee062e797d5ec5
UtnValure <vlaure@alu.frlp.utn.edu.ar>
2024-10-15 19:05:06 -0300

    POST NO ANDA

--- Persicuf/Program.cs
+++ Persicuf/Program.cs
@@ -1,5 +1,7 @@
 using DB.Data;
 using Microsoft.EntityFrameworkCore;
+using Servicios.Servicios;
+using Servicios.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,30 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<PersicufContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddControllers();
+builder.Services.AddScoped<IColorServicio, ColorServicio>();
+builder.Services.AddScoped<ICamperaServicio, CamperaServicio>();
+builder.Services.AddScoped<ICorteCuelloServicio, CorteCuelloServicio>();
+builder.Services.AddScoped<IDomicilioServicio, DomicilioServicio>();
+builder.Services.AddScoped<IImagenServicio, ImagenServicio>();
+builder.Services.AddScoped<ILargoServicio, LargoServicio>();
+builder.Services.AddScoped<ILocalidadServicio, LocalidadServicio>();
+builder.Services.AddScoped<IMangaServicio, MangaServicio>();
+builder.Services.AddScoped<IMaterialServicio, MaterialServicio>();
+builder.Services.AddScoped<IPantalonServicio, PantalonServicio>();
+builder.Services.AddScoped<IPedidoPrendaServicio, PedidoPrendaServicio>();
+builder.Services.AddScoped<IPedidoServicio, PedidoServicio>();
+builder.Services.AddScoped<IPermisoServicio, PermisoServicio>();
+builder.Services.AddScoped<IPrendaEstampadoServicio, PrendaEstampadoServicio>();
+builder.Services.AddScoped<IPrendaServicio, PrendaServicio>();
+builder.Services.AddScoped<IProvinciaServicio, ProvinciaServicio>();
+builder.Services.AddScoped<IRemeraServicio, RemeraServicio>();
+builder.Services.AddScoped<IRubroServicio, RubroServicio>();
+builder.Services.AddScoped<ITalleAlfabeticoServicio, TalleAlfabeticoServicio>();
+builder.Services.AddScoped<ITalleNumericoServicio, TalleNumericoServicio>();
+builder.Services.AddScoped<IUbicacionServicio, UbicacionServicio>();
+builder.Services.AddScoped<IUsuarioServicio, UsuarioServicio>();
+builder.Services.AddScoped<IZapatoServicio, ZapatoServicio>();
 var app = builder.Build();
 
 /*using (var scope = app.Services.CreateScope())
